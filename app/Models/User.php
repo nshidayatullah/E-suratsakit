8cef263a3b1e5e39686be1ce37087119a0e93706
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2025-12-12 23:36:25 +0800

    fixing panel bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
